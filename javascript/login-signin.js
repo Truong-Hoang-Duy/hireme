e635f120126a7d2f48be3e039bb642942e7903c2
Truong Hoang Duy <truonghoangduy2612@gmail.com>
2024-12-30 08:52:19 +0700

    first commit

---
+++ javascript/login-signin.js
@@ -0,0 +1,19 @@
+let formOpenBtn = document.querySelector("#form-open");
+let loginContainer = document.querySelector(".login-container");
+let formContainer = document.querySelector(".form-container");
+let formCloseBtn = document.querySelector(".form-close");
+let signupBtn = document.querySelector("#signup");
+let loginBtn = document.querySelector("#login");
+let pwShowHide = document.querySelector(".hide");
+// show-up
+// formOpenBtn.addEventListener("click",() => loginContainer.classList.add("show"))
+// formOpenBtn.addEventListener("click",() => loginContainer.classList.remove("show"))
+// changing-form
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
